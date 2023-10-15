749161d69155d4b0d49cc459c170fb3f8cd24910
SIDs1de <82277183+SIDs1de@users.noreply.github.com>
2023-10-15 16:12:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  
+  function wow() {
+    wow = new WOW({
+      live: false, // default
+    });
+    wow.init();
+  }
+
+
+  wow();
+});
